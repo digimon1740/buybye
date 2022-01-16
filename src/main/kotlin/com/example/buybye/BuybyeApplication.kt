6201736eb1721817c86536b7bc63b96b2b534356
sanghoon <digimon.1740@gmail.com>
2022-01-16 22:01:38 +0900

    init

---
+++ src/main/kotlin/com/example/buybye/BuybyeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.buybye
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BuybyeApplication
+
+fun main(args: Array<String>) {
+    runApplication<BuybyeApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/buybye/BuybyeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.buybye
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BuybyeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
