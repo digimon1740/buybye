0146672605af48fe171c7956aca692ac27683f4b
sanghoon <digimon.1740@gmail.com>
2022-01-17 01:02:38 +0900

    5일 이동 평균선

--- src/main/kotlin/com/example/buybye/controller/TradeController.kt
+++ src/main/kotlin/com/example/buybye/controller/TradeController.kt
@@ -1,6 +1,8 @@
 package com.example.buybye.controller
 
 import com.example.buybye.auth.AuthTokenGenerator
+import com.example.buybye.domain.candle.Candle
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpHeaders
 import org.springframework.web.bind.annotation.GetMapping
@@ -12,6 +14,7 @@ import org.springframework.web.reactive.function.client.awaitBody
 @RestController
 class TradeController(
     private val authTokenGenerator: AuthTokenGenerator,
+    private val objectMapper: ObjectMapper,
 ) {
 
     private val log = LoggerFactory.getLogger(javaClass)
@@ -39,7 +42,7 @@ class TradeController(
         @RequestParam(defaultValue = "1") minuteUnit: String,
         @RequestParam(defaultValue = "KRW-BTC") market: String,
         @RequestParam(defaultValue = "200") count: Int,
-    ): String {
+    ): List<Candle> {
         val jwt = authTokenGenerator.generateJwt()
         val url = if (periodUnit == "minutes") {
             "https://api.upbit.com/v1/candles/${periodUnit}/${minuteUnit}"
@@ -55,11 +58,35 @@ class TradeController(
             .build()
             .get()
             .retrieve()
-            .awaitBody<String>()
+            .awaitBody<List<Candle>>()
         return response
     }
 
     // 매수 목표가 조회
 
+    // 이동 평균 조회
+    @GetMapping("/v1/ma")
+    suspend fun getMovingAverage(
+        @RequestParam(defaultValue = "days") periodUnit: String,
+        @RequestParam(defaultValue = "KRW-BTC") market: String,
+        @RequestParam(defaultValue = "5") count: Int,
+    ): Map<String, Any> {
+        val jwt = authTokenGenerator.generateJwt()
+        val baseUrl = "https://api.upbit.com/v1/candles/${periodUnit}?market=${market}&count=${count}"
+        val response = WebClient.builder()
+            .baseUrl(baseUrl)
+            .defaultHeaders {
+                it[HttpHeaders.AUTHORIZATION] = "Bearer $jwt"
+                it[HttpHeaders.CONTENT_TYPE] = "application/json"
+            }
+            .build()
+            .get()
+            .retrieve()
+            .awaitBody<List<Candle>>()
+
+        val avg = response.sumOf { it.trade_price } / count
+
+        return mapOf("avg of 5" to avg)
+    }
 
 }
